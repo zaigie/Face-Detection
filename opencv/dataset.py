7fdea2e3c822b9ef230cacf2b11cf002ef587ed5
JoyceWedamson <jokerwho@yeah.net>
2019-04-21 00:17:07 +0800

    Update dataset.py

--- opencv/dataset.py
+++ opencv/dataset.py
@@ -7,7 +7,7 @@ cam.set(4, 480) # set video height
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-face_id = input('\n 输入用户ID，如1,2,3...然后回车 ==>  ')
+name = input('\n 输入姓名（英文），然后回车 ==>  ')
 
 print("\n [提示] 正在进行人脸采集，请看向摄像头并等待...")
 count = 0
@@ -24,16 +24,19 @@ while(True):
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
 
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("dataset/" + str(name) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
         cv2.imshow('image', img)
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    k = cv2.waitKey(100) & 0xff
     if k == 27:
         break
-    elif count >= 30: # Take 30 face sample and stop video
+    elif count >= 30:
          break
 
+f = open('facedata.txt','a')
+f.write(str(name) + '\n')
+f.close()
 print("\n [提示] 正在退出程序...")
 cam.release()
 cv2.destroyAllWindows()
